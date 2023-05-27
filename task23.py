f9c574de886de67c35aa6f2dde98a388ff40e37e
Julia <matterfixed@gmail.com>
2023-05-27 17:05:55 +0300

    done

---
+++ task23.py
@@ -0,0 +1,18 @@
+# Задача №23. Дан массив, состоящий из целых чисел.
+# Напишите программу,которая подсчитает количество элементов
+# массива, больших предыдущего (элемента с предыдущим номером)
+
+# 1
+from random import randint
+my_list = []
+lens = 10
+for i in range(10):
+    my_list.append(randint(0, 10))
+print(my_list)
+count = 0
+for i in range(len(my_list) - 1):
+    if my_list[i + 1] > my_list
+
+# 2
+my_dict = [{}]
+
